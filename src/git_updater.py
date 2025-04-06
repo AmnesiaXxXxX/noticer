0ae20ff71b37f9d7cfc90fdad0ddb92f50ccba0b
amnesiawho <92972232+AmnesiaXxXxX@users.noreply.github.com>
2025-04-06 19:33:43 +0000

    refactor: Удалить асинхронный генератор и добавить перезапуск приложения

--- src/git_updater.py
+++ src/git_updater.py
@@ -12,6 +12,8 @@ class GitUpdater:
         self.logger = logging.getLogger("gitupdater")
         self.logger.level = logging.INFO
         self.logger.info(f"Last Git Update: {self.get_last_git_update()}")
+        self.is_generator_enabled: bool = True
+        self.loop: asyncio.AbstractEventLoop | None = None
 
     def get_last_git_update(self) -> datetime | None:
         try:
@@ -84,44 +86,9 @@ class GitUpdater:
         try:
             self.logger.info("Restarting application...")
             subprocess.run(["poetry", "run", "run"], check=True)
+            exit()
         except subprocess.CalledProcessError as e:
             self.logger.error(f"Error while restarting the application: {e}")
-
-    def __enter__(self):
-        self.logger.info("Entering context manager.")
-        if not self.loop:
-            self.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.loop)
-            self.loop.create_task(self.generator_enter())
-            self.loop.run_in_executor(None, self.loop.run_forever)
-        return self
-
-    def __exit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_value: BaseException | None,
-        traceback: object | None,
-    ) -> None:
-        self.logger.info("Exiting context manager.")
-        self.is_generator_enabled = False
-        if self.loop and self.loop.is_running():  # type: ignore[union-attr]
-            self.loop.call_soon_threadsafe(self.loop.stop)
-
-
-updater = GitUpdater()
-if __name__ == "__main__":
-    with updater:
-        while True:
-            time.sleep(1)
-
-    async def generator_enter(self):
-        self.logger.info(self.is_generator_enabled)
-
-        while self.is_generator_enabled:
-            if not self.is_latest_version:
-                self.update()
-            self.logger.info(self.get_last_git_update())
-            await asyncio.sleep(20)
 
     def __enter__(self):
         self.logger.info("Entering context manager.")
