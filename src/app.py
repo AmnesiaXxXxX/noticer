905f60e3daf04b7f0806110b8f4e329017955c4a
amnesiawho <92972232+AmnesiaXxXxX@users.noreply.github.com>
2025-04-06 19:02:55 +0000

    refactor: Переместить создание экземпляра бота в функцию запуска

--- src/app.py
+++ src/app.py
@@ -13,15 +13,20 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 API_ID = os.getenv("API_ID", "")
 API_HASH = os.getenv("API_HASH", "")
 updater = GitUpdater()
+bot = Bot("reminder_bot", API_ID, API_HASH, BOT_TOKEN)
 
 
 def run():
     """Функция для запуска через poetry run app:run"""
     if not updater.is_latest_version:
-        if input(f"Обнаружена новая версия ({updater.get_last_git_update()}), обновить? (y/n): ") == "y":
+        if (
+            input(
+                f"Обнаружена новая версия ({updater.get_last_git_update()}), обновить? (y/n): "
+            )
+            == "y"
+        ):
             updater.update()
         else:
             pass
 
-    bot = Bot("reminder_bot", API_ID, API_HASH, BOT_TOKEN)
     bot.launch_bot()
